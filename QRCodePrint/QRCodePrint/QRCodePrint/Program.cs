f584918f9ab19c5a35c609da4609c1469870ca5b
boby16 <yangzhongbo16@aliyun.com>
2018-01-28 14:13:32 +0800

    二维码打印

---
+++ QRCodePrint/QRCodePrint/QRCodePrint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+
+namespace QRCodePrint
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new main());
+        }
+    }
+}
